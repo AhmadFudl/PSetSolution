19f5d44b7d98ffe82ee700b43557d42275419680
Fudl <notfudl@gmail.coma>
2022-06-08 00:25:00 +0200

    A new convention for naming functions

--- template/temp.cpp
+++ template/temp.cpp
@@ -6,14 +6,14 @@
 
 #define ussio std::cin.tie(0), std::cin.sync_with_stdio(0)
 #define endl '\n'
-#define Multiple_Tests 1
-#define Single_Test 0
+#define MULTIPLE_TESTS 1
+#define SINGLE_TEST 0
 
-void set(bool);
+void problem_Type(bool);
 // NUM_THEORY_HEADERS
 
 
-void process() // TODO: Find more appropriate name
+void duck_It() // TODO: Find more appropriate name
 {
 	using namespace std;
 	return;
@@ -25,15 +25,15 @@ int main()
 	#ifndef ONLINE_JUDGE
 		freopen("in.io", "r", stdin);
 	#endif
-	set(Single_Test);
+	problem_Type(SINGLE_TEST);
 	return 0;
 }
 
-void set(bool a)
+void problem_Type(bool a)
 {
 	int t = 1;
-	if (a) std::cin >> t;
-	while (t--) process();
+	a && std::cin >> t;
+	while (t--) duck_It();
 }
 
 // NUM_THEORY_FUNCTIONS
