28543ebd9c0ed5c65147fadcec331bc7c834bc8c
Fudl <notfudl@gmail.coma>
2022-06-24 18:57:38 +0200

    Another meaningles change to the template. Like if that gona make me more productive or better at cp

--- template/temp.cpp
+++ template/temp.cpp
@@ -2,38 +2,41 @@
 * A work in progress template for CPC
 */
 
+#include <cstdlib>
 #include <iostream>
 
-#define ussio std::cin.tie(0), std::cin.sync_with_stdio(0)
 #define endl '\n'
 #define MULTIPLE_TESTS 1
 #define SINGLE_TEST 0
 
-void problem_Type(bool);
-// NUM_THEORY_HEADERS
+using namespace std;
 
+void Test(bool);
+// NUM_THEORY_HEADERS
 
 void duck_It() // TODO: Find more appropriate name
 {
-	using namespace std;
-	return;
+  // TODO solve:
 }
 
 int main()
 {
-	ussio;
-	#ifndef ONLINE_JUDGE
-		freopen("in.io", "r", stdin);
-	#endif
-	problem_Type(SINGLE_TEST);
-	return 0;
+  cin.tie(0);
+  cin.sync_with_stdio(0);
+  #ifndef ONLINE_JUDGE
+    freopen("in.io", "r", stdin);
+  #endif
+
+  Test(SINGLE_TEST);
+
+  return EXIT_SUCCESS;
 }
 
-void problem_Type(bool a)
+void Test(bool a)
 {
-	int t = 1;
-	a && std::cin >> t;
-	while (t--) duck_It();
+  int t = 1;
+  a && cin >> t;
+  while (t--) duck_It();
 }
 
 // NUM_THEORY_FUNCTIONS
